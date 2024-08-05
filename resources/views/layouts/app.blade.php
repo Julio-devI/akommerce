71424cb83e9448f71aee080a769303110a08234b
julio-DevI <julio.a.valenca@gmail.com>
2024-08-05 15:14:11 -0300

    feat: add the AuthAdmin mdidleware

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com/">
     <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Allura&amp;display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('assets/css/plugins/swiper.min.css" type="text/css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/plugins/swiper.min.css') }}" type="text/css" />
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}" type="text/css" />
     <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}" type="text/css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
@@ -387,7 +387,7 @@
             <div class="header-desk header-desk_type_1">
                 <div class="logo">
                     <a href="index.html">
-                        <img src="{{ asset('assets/images/logo.png') }}'" alt="Uomo" class="logo__image d-block" />
+                        <img src="{{ asset('assets/images/logo.png') }}" alt="Uomo" class="logo__image d-block" />
                     </a>
                 </div>
 
