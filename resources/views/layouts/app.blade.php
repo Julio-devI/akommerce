e7ec9b817168943678cf04a9a52078d108f30de5
julio-DevI <julio.a.valenca@gmail.com>
2025-02-19 15:34:17 -0300

    feat: edit directives

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -631,7 +631,7 @@
 
         <div class="footer-bottom">
             <div class="container d-md-flex align-items-center">
-                <span class="footer-copyright me-auto">©2024 Surfside Media</span>
+                <span class="footer-copyright me-auto">©2025 JulioDevI</span>
                 <div class="footer-settings d-md-flex align-items-center">
                     <a href="privacy-policy.html">Privacy Policy</a> &nbsp;|&nbsp; <a href="terms-conditions.html">Terms &amp; Conditions</a>
                 </div>
